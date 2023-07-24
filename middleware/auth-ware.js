dccff0e3d00789778552d74252f11b1898613dc1
Daniel Tressel <dhtressel@gmail.com>
2023-07-24 08:19:28 -0400

    fix ensure correct user test

--- middleware/auth-ware.js
+++ middleware/auth-ware.js
@@ -68,7 +68,7 @@ function ensureAdmin(req, res, next) {
 function ensureCorrectUserOrAdmin(req, res, next) {
   try {
     const user = res.locals.user;
-    if (!(user && (user.permissions === "admin" || user.id === req.params.userId))) {
+    if (!(user && (user.permissions === "admin" || user.userId === +req.params.userId))) {
       throw new UnauthorizedError();
     }
     return next();
