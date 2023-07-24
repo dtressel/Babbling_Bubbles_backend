72cc2176dbe7a4909cc5de697988f4a4475924ea
Daniel Tressel <dhtressel@gmail.com>
2023-07-23 22:35:37 -0400

    fixed syntax errors

--- database-sql/bb-schema.sql
+++ database-sql/bb-schema.sql
@@ -8,7 +8,7 @@ CREATE TABLE users
   first_name VARCHAR NOT NULL,
   last_name VARCHAR NOT NULL,
   date_registered DATE NOT NULL DEFAULT CURRENT_DATE,
-  permissions VARCHAR NOT NULL DEFAULT "base",
+  permissions VARCHAR NOT NULL DEFAULT 'base',
   peak_10_wma FLOAT NOT NULL DEFAULT 0,
   peak_100_wma FLOAT NOT NULL DEFAULT 0,
   curr_10_wma FLOAT NOT NULL DEFAULT 0,
@@ -30,5 +30,5 @@ CREATE TABLE plays
   best_word VARCHAR,
   best_word_score INT,
   best_word_board_state VARCHAR,
-  FOREIGN KEY (user_id) REFERENCES User(id)
+  FOREIGN KEY (user_id) REFERENCES users(id)
 );
