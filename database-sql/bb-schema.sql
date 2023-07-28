1048e683066927bc18cc4c1f63875656d2a11db8
Daniel Tressel <dhtressel@gmail.com>
2023-07-27 23:28:12 -0400

    added new stats to users, changed some data types

--- database-sql/bb-schema.sql
+++ database-sql/bb-schema.sql
@@ -1,34 +1,41 @@
 CREATE TABLE users
 (
   id SERIAL PRIMARY KEY,
-  username VARCHAR NOT NULL,
-  password VARCHAR NOT NULL,
-  email VARCHAR NOT NULL,
-  country VARCHAR,
-  first_name VARCHAR,
-  last_name VARCHAR,
+  username TEXT NOT NULL,
+  password TEXT NOT NULL,
+  email TEXT NOT NULL,
+  country TEXT,
+  first_name TEXT,
+  last_name TEXT,
   date_registered DATE NOT NULL DEFAULT CURRENT_DATE,
-  permissions VARCHAR NOT NULL DEFAULT 'base',
-  peak_10_wma FLOAT,
-  peak_100_wma FLOAT,
+  permissions TEXT NOT NULL DEFAULT 'base',
   curr_10_wma FLOAT,
   curr_100_wma FLOAT,
+  peak_10_wma FLOAT,
+  peak_100_wma FLOAT,
   num_of_plays_single INT NOT NULL DEFAULT 0,
-  last_play_single DATE
+  last_play_single DATE,
+  longest_word TEXT,
+  longest_word_score SMALLINT,
+  craziest_word TEXT,
+  craziest_word_score SMALLINT,
+  tenth_best_score INT,
+  tenth_best_avg_word_score FLOAT,
+  tenth_best_best_word_score INT
 );
 
 CREATE TABLE plays
 (
-  id SERIAL PRIMARY KEY,
+  id BIGSERIAL PRIMARY KEY,
   user_id INT NOT NULL,
-  game_type INT NOT NULL DEFAULT 0,
+  game_type SMALLINT NOT NULL DEFAULT 0,
   game_id INT,
   play_time TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP(0),
   score INT NOT NULL,
-  num_of_words INT NOT NULL,
+  num_of_words SMALLINT NOT NULL,
   avg_word_score FLOAT,
-  best_word VARCHAR,
+  best_word TEXT,
   best_word_score INT,
-  best_word_board_state VARCHAR,
+  best_word_board_state TEXT,
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
