4f1880a0d88009966af614b22c2a881b994195e5
Daniel Tressel <dhtressel@gmail.com>
2023-10-06 12:14:20 -0400

    finished leaderboard routes and model

--- models/leaderboard-model.js
+++ models/leaderboard-model.js
@@ -10,8 +10,57 @@ const {
 /** Related functions for leaderboards. */
 
 class Leaderboard {
+  static gameTypes = ['solo3', 'solo10', 'free'];
+  static wmaPeriods = ['20', '100'];
+  static wmaStatTypes = ['curr', 'peak'];
+  static bestScoreTypes = ['ttl', 'avg'];
+  static bestWordTypes = ['bst', 'crz', 'lng'];
+  static userFiltersKey = { userIds: 'user_id', usernames: 'username' }
+
+
   /*
-    Get leaderboards by game type
+    Method to build where clause additions in case of user filters
+
+    Builds a where clause such as "AND (user_id = $2 OR username = $3)"
+
+    Adds values to the valuesArray for new arguments
+  */
+  static buildUserWhereAddition(filters, valuesArray) {
+    let whereString = '';
+    let firstClause = true;
+
+    const userFilters = {};
+    if (filters.userIds) userFilters.userIds = filters.userIds;
+    if (filters.usernames) userFilters.usernames = filters.usernames;
+
+    // build whereString and valuesArray for each filter
+    for (const filter in userFilters) {
+      // userFilters value must always be an array
+      for (const value of userFilters[filter]) {
+        if (firstClause) {
+          whereString = `AND (${this.userFiltersKey[filter]} = $${valuesArray.length + 1}`
+          firstClause = false;
+        } else {
+          if (whereString) whereString += ' ';
+          whereString += `OR ${this.userFiltersKey[filter]} = $${valuesArray.length + 1}`
+        }
+        valuesArray.push(value);
+      }
+    }
+    // close parenthesis
+    if (whereString) whereString += ')';
+    return { whereClause: whereString, valuesArray }
+  }
+
+
+  /*
+    Get leaderboards
+
+    Can filter by:
+    - gameTypes
+    - statNames
+    - userIds
+    - usernames
 
     example Return
     {
@@ -21,19 +70,32 @@ class Leaderboard {
     }
   */
 
-  static async getByType(filters) {
-    const gameType = filters.gameType;
-    const wmas = ['20', '100'];
-    const wmaStatTypes = ['curr', 'peak'];
-    const bestScoreTypes = ['ttl', 'avg'];
-    const bestWordTypes = ['bst', 'crz', 'lng'];
+
+  static async get(filters) {
+    // use alternative method if there is a statName filter
+    if (filters.statNames) {
+      const promises = [];
+      let leaderboards = {};
+      // get leaderboard results for each stat name
+      for (const statName of filters.statNames) {
+        promises.push(this.getByStatName(statName, filters));
+      }
+      const results = await Promise.all(promises);
+      // add all leaderboard results to leaderboard object
+      for (const result of results) {
+        leaderboards = { ...leaderboards, ...result };
+      }
+
+      return leaderboards;
+    }
+
+    // set game types
+    const gameTypes = [];
+    if (filters.gameTypes) gameTypes.push(...filters.gameTypes);
+    else gameTypes.push(...this.gameTypes);
 
     // initialize promise object to gather promises to await later
-    const promises = {
-      wmaStats: {},
-      bestScores: {},
-      bestWords: {}
-    }
+    const promises = {};
 
     // initialize leaderboard object for return
     const leaderboards = {};
@@ -49,65 +111,187 @@ class Leaderboard {
         bestWords: { bst: <promise>, ... }
       }
     */
-    for (const wma of wmas) {
-      for (const statType of wmaStatTypes) {
-        promises.wmaStats[`${statType}${wma}Wma`] = db.query(
+
+    for (const type of this.bestScoreTypes) {
+      promises[`best${type[0].toUpperCase()}${type.slice(1)}Score`] = {};
+      for (const gameType of gameTypes) {
+        // build where clause addition in case of userId or username filters
+        const { whereClause, valuesArray } = this.buildUserWhereAddition(filters, [gameType, type]);
+
+        promises[`best${type[0].toUpperCase()}${type.slice(1)}Score`][gameType] = db.query(
           `
             SELECT u.username,
-                   ss.game_type AS "gameType",
-                   ss.${statType}_${wma}_wma AS "${statType}${wma}Wma"
-                   ${statType === 'peak' ? `, TO_CHAR(ss.peak_${wma}_wma_date, 'Mon DD, YYYY') AS "peak${wma}WmaDate"` : ''}
+                   bs.score,
+                   TO_CHAR(bs.acheived_on, 'Mon DD, YYYY') AS "date"
+            FROM best_scores AS "bs"
+            INNER JOIN users AS "u"
+              ON bs.user_id = u.id
+            WHERE bs.game_type = $1
+              AND bs.score_type = $2
+              ${whereClause}
+            ORDER BY bs.score DESC
+            LIMIT 10
+          `,
+          valuesArray 
+        );
+      }
+    }
+    for (const type of this.bestWordTypes) {
+      promises[`${type}Word`] = {};
+      for (const gameType of gameTypes) {
+        // build where clause addition in case of userId or username filters
+        const { whereClause, valuesArray } = this.buildUserWhereAddition(filters, [gameType, type]);
+        promises[`${type}Word`][gameType] = db.query(
+          `
+            SELECT u.username,
+                   bw.word,
+                   bw.score,
+                   bw.board_state AS "boardState",
+                   TO_CHAR(bw.found_on, 'Mon DD, YYYY') AS "date"
+            FROM best_words AS "bw"
+            INNER JOIN users AS "u"
+              ON bw.user_id = u.id
+            WHERE bw.game_type = $1
+              AND bw.best_type = $2
+              ${whereClause}
+            ORDER BY bw.score DESC
+            LIMIT 10
+          `,
+          valuesArray 
+        );
+      }
+    }
+    // only get wmas if not filtering by user
+    if (!filters.userIds && !filters.usernames) {
+      for (const wma of this.wmaPeriods) {
+        for (const statType of this.wmaStatTypes) {
+          promises[`${statType}${wma}Wma`] = {};
+          for (const gameType of gameTypes) {
+            promises[`${statType}${wma}Wma`][gameType] = db.query(
+              `
+                SELECT u.username,
+                       ss.${statType}_${wma}_wma AS "${statType}${wma}Wma"
+                       ${statType === 'peak' ? `, TO_CHAR(ss.peak_${wma}_wma_date, 'Mon DD, YYYY') AS "peak${wma}WmaDate"` : ''}
+                FROM solo_stats AS "ss"
+                INNER JOIN users AS "u"
+                  ON ss.user_id = u.id
+                WHERE ss.game_type = $1
+                  AND ss.${statType}_${wma}_wma > 0
+                ORDER BY ss.${statType}_${wma}_wma DESC
+                LIMIT 10
+              `,
+              [gameType]
+            );
+          }
+        }
+      }
+    }
+
+    /* 
+      build leaderboard object and add result rows after awaiting
+      leaderboard object will be structured like the return object example above this method
+    */
+    for (const category in promises) {
+      leaderboards[category] = {};
+      for (const statType in promises[category]) {
+        const results = await promises[category][statType];
+        leaderboards[category][statType] = results.rows;
+      }
+    }
+
+    return leaderboards;
+  }
+  
+
+  static async getByStatName(statName, filters) {
+    // set game types
+    const gameTypes = [];
+    if (filters.gameTypes) gameTypes.push(...filters.gameTypes);
+    else gameTypes.push(...this.gameTypes);
+
+    // initialize promise object to gather promises to await later
+    const promises = {};
+
+    // initialize leaderboard object for return
+    const leaderboards = {};
+
+    if (statName.includes("Wma")) {
+      if (filters.userIds || filters.usernames) {
+        throw new BadRequestError("Wma stats should not be retrieved when filtering by userIds or usernames. Please remove any wma stats from the statName list.");
+      }
+      promises[statName] = {};
+      for (const gameType of gameTypes) {
+        const peakOrCurr = statName.slice(0, 4);
+        const wmaPeriod = statName.slice(4, -3);
+        promises[statName][gameType] = db.query(
+          `
+            SELECT u.username,
+                   ss.${peakOrCurr}_${wmaPeriod}_wma AS "${statName}"
+                   ${peakOrCurr === 'peak' ? `, TO_CHAR(ss.peak_${wmaPeriod}_wma_date, 'Mon DD, YYYY') AS "peak${wmaPeriod}WmaDate"` : ''}
             FROM solo_stats AS "ss"
             INNER JOIN users AS "u"
               ON ss.user_id = u.id
             WHERE ss.game_type = $1
-              AND ss.${statType}_${wma}_wma > 0
-            ORDER BY ss.${statType}_${wma}_wma DESC
+              AND ss.${peakOrCurr}_${wmaPeriod}_wma > 0
+            ORDER BY ss.${peakOrCurr}_${wmaPeriod}_wma DESC
             LIMIT 10
           `,
           [gameType]
         );
       }
     }
-    for (const type of bestScoreTypes) {
-      promises.bestScores[type] = db.query(
-        `
-          SELECT u.username,
-                 bs.game_type AS "gameType",
-                 bs.score_type AS "scoreType",
-                 bs.score,
-                 TO_CHAR(bs.acheived_on, 'Mon DD, YYYY') AS "date"
-          FROM best_scores AS "bs"
-          INNER JOIN users AS "u"
-            ON bs.user_id = u.id
-          WHERE bs.game_type = $1
-            AND bs.score_type = '${type}'
-          ORDER BY bs.score DESC
-          LIMIT 10
-        `,
-        [gameType] 
-      );
+    else if (statName.includes("Score")) {
+      promises[statName] = {};
+      const scoreType = statName.slice(4, 7).toLowerCase();
+      for (const gameType of gameTypes) {
+        // build where clause addition in case of userId or username filters
+        const { whereClause, valuesArray } = this.buildUserWhereAddition(filters, [gameType, scoreType]);
+        promises[statName][gameType] = db.query(
+          `
+            SELECT u.username,
+                   bs.score,
+                   TO_CHAR(bs.acheived_on, 'Mon DD, YYYY') AS "date"
+            FROM best_scores AS "bs"
+            INNER JOIN users AS "u"
+              ON bs.user_id = u.id
+            WHERE bs.game_type = $1
+              AND bs.score_type = $2
+              ${whereClause}
+            ORDER BY bs.score DESC
+            LIMIT 10
+          `,
+          valuesArray 
+        );
+      }
     }
-    for (const type of bestWordTypes) {
-      promises.bestWords[type] = db.query(
-        `
-          SELECT u.username,
-                 bw.game_type AS "gameType",
-                 bw.best_type AS "bestType",
-                 bw.word,
-                 bw.score,
-                 bw.board_state AS "boardState",
-                 TO_CHAR(bw.found_on, 'Mon DD, YYYY') AS "date"
-          FROM best_words AS "bw"
-          INNER JOIN users AS "u"
-            ON bw.user_id = u.id
-          WHERE bw.game_type = $1
-            AND bw.best_type = '${type}'
-          ORDER BY bw.score DESC
-          LIMIT 10
-        `,
-        [gameType] 
-      );
+    else if (statName.includes("Word")) {
+      promises[statName] = {};
+      const bestType = statName.slice(0, 3);
+      for (const gameType of gameTypes) {
+        // build where clause addition in case of userId or username filters
+        const { whereClause, valuesArray } = this.buildUserWhereAddition(filters, [gameType, bestType]);
+        promises[statName][gameType] = db.query(
+          `
+            SELECT u.username,
+                   bw.word,
+                   bw.score,
+                   bw.board_state AS "boardState",
+                   TO_CHAR(bw.found_on, 'Mon DD, YYYY') AS "date"
+            FROM best_words AS "bw"
+            INNER JOIN users AS "u"
+              ON bw.user_id = u.id
+            WHERE bw.game_type = $1
+              AND bw.best_type = $2
+              ${whereClause}
+            ORDER BY bw.score DESC
+            LIMIT 10
+          `,
+          valuesArray 
+        );
+      }
+    }
+    else {
+      throw BadRequestError(`${statType} is not a valid stat type`);
     }
 
     /* 
--- routes/leaderboards.js
+++ routes/leaderboards.js
@@ -7,26 +7,188 @@ const jsonschema = require("jsonschema");
 const express = require("express");
 const Leaderboard = require("../models/leaderboard-model");
 const leaderboardGetSchema = require("../schemas/leaderboardGet.json");
+const { BadRequestError } = require("../expressError");
 
 const router = express.Router();
 
+
 /** GET / => 
  * 
- *  can choose game type for displayed leaderboards with query parameter:
+ *  can filter with the following query parameter:
  *  
- *    -gameType = solo3, solo10, free (solo3 is default) 
+ *    - gameTypes = a string or an array with one or all of the following values: 
+ *        solo3, solo10, free
+ *    - statNames = a string or an array with one or all of the following values: 
+ *        curr20Wma, peak20Wma, curr100Wma, peak100Wma, bestTtlScore, bestAvgScore, bstWord, crzWord, lngWord
+ *    - userIds = an integer or an array with one or many userIds
+ *    - usernames = a string or an array with one or many usernames
  * 
- *  { 
- *    leaderboards: {
- *      bestAvgWordScoreMin15: [{playId, username, playTime, avgWordScore}, {...}, ...],
- *      bestCurrent100Wma: [{playId, username, curr100Wma}, {...}, ...],
- *      bestCurrent10Wma: [{playId, username, curr10Wma}, {...}, ...],
- *      bestPeak100Wma: [{playId, username, peak100Wma}, {...}, ...],
- *      bestPeak10Wma: [{playId, username, peak10Wma}, {...}, ...],
- *      bestPlayScoresSingle: [{playId, username, playTime, score}, {...}, ...],
- *      bestWordScores: [{playId, username, playTime, bestWord, bestWordScore}, {...}, ...]
- *    } 
- *  }
+    {
+      "leaderboards": {
+        "curr20Wma": {
+          "solo3": [],
+          "solo10": [],
+          "free": []
+        },
+        "peak20Wma": {
+          "solo3": [],
+          "solo10": [],
+          "free": []
+        },
+        "curr100Wma": {
+          "solo3": [],
+          "solo10": [],
+          "free": []
+        },
+        "peak100Wma": {
+          "solo3": [],
+          "solo10": [],
+          "free": []
+        },
+        "bestTtlScore": {
+          "solo3": [
+            {
+              "username": "bubbles",
+              "score": 256,
+              "date": "Oct 02, 2023"
+            }
+          ],
+          "solo10": [
+            {
+              "username": "bubbles",
+              "score": 456,
+              "date": "Oct 02, 2023"
+            }
+          ],
+          "free": [
+            {
+              "username": "bubbles",
+              "score": 256,
+              "date": "Oct 02, 2023"
+            }
+          ]
+        },
+        "bestAvgScore": {
+          "solo3": [
+            {
+              "username": "bubbles",
+              "score": 11.13,
+              "date": "Oct 02, 2023"
+            }
+          ],
+          "solo10": [
+            {
+              "username": "bubbles",
+              "score": 12.67,
+              "date": "Oct 02, 2023"
+            }
+          ],
+          "free": [
+            {
+              "username": "bubbles",
+              "score": 132.31,
+              "date": "Oct 02, 2023"
+            },
+            {
+              "username": "bubbles",
+              "score": 11.13,
+              "date": "Oct 02, 2023"
+            }
+          ]
+        },
+        "bstWord": {
+          "solo3": [
+            {
+              "username": "bubbles",
+              "word": "garden",
+              "score": 80,
+              "boardState": "ABcDefghijklmnopqrst122",
+              "date": "Oct 02, 2023"
+            }
+          ],
+          "solo10": [
+            {
+              "username": "bubbles",
+              "word": "garden",
+              "score": 80,
+              "boardState": "ABcDefghijklmnopqrst122",
+              "date": "Oct 02, 2023"
+            }
+          ],
+          "free": [
+            {
+              "username": "bubbles",
+              "word": "garden",
+              "score": 680,
+              "boardState": "ABcDefghijklmnopqrst122",
+              "date": "Oct 02, 2023"
+            },
+            {
+              "username": "bubbles",
+              "word": "garden",
+              "score": 680,
+              "boardState": "ABcDefghijklmnopqrst122",
+              "date": "Oct 02, 2023"
+            },
+            {
+              "username": "bubbles",
+              "word": "garden",
+              "score": 80,
+              "boardState": "ABcDefghijklmnopqrst122",
+              "date": "Oct 02, 2023"
+            }
+          ]
+        },
+        "crzWord": {
+          "solo3": [],
+          "solo10": [],
+          "free": []
+        },
+        "lngWord": {
+          "solo3": [
+            {
+              "username": "bubbles",
+              "word": "carpenter",
+              "score": 9034,
+              "boardState": "ABcDefghijklmnopqrst122",
+              "date": "Oct 02, 2023"
+            }
+          ],
+          "solo10": [
+            {
+              "username": "bubbles",
+              "word": "carpenter",
+              "score": 9034,
+              "boardState": "ABcDefghijklmnopqrst122",
+              "date": "Oct 02, 2023"
+            }
+          ],
+          "free": [
+            {
+              "username": "bubbles",
+              "word": "carpenter",
+              "score": 9034,
+              "boardState": "ABcDefghijklmnopqrst122",
+              "date": "Oct 02, 2023"
+            },
+            {
+              "username": "bubbles",
+              "word": "carpenter",
+              "score": 9034,
+              "boardState": "ABcDefghijklmnopqrst122",
+              "date": "Oct 02, 2023"
+            },
+            {
+              "username": "bubbles",
+              "word": "carpenter",
+              "score": 9034,
+              "boardState": "ABcDefghijklmnopqrst122",
+              "date": "Oct 02, 2023"
+            }
+          ]
+        }
+      }
+    }
  *
  * Returns all leaderboards up to 10 entries per leaderboard.
  *
@@ -36,13 +198,22 @@ const router = express.Router();
 router.get("/", async function (req, res, next) {
   try {
     const filters = req.query;
+    // if a single value was submitted rather than multiple, coerce into an array
+    for (const filter in filters) {
+      if (!Array.isArray(filters[filter])) {
+        filters[filter] = [filters[filter]];
+      }
+    }
+    // coerce strings into integers for userIds
+    if (filters.userIds) {
+      filters.userIds = filters.userIds.map(id => +id);
+    }
     const validator = jsonschema.validate(filters, leaderboardGetSchema);
     if (!validator.valid) {
       const errs = validator.errors.map(e => e.stack);
       throw new BadRequestError(errs);
     }
-    if (!filters.gameType) filters.gameType = "solo3";
-    const leaderboards = await Leaderboard.getByType(filters);
+    const leaderboards = await Leaderboard.get(filters);
     return res.json({ leaderboards });
   } catch (err) {
     return next(err);
